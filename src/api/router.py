ae9ee6cfd30318f54922ef3d336341069d181076
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-03-27 17:52:33 -0300

    [feature] httpexception response, if routine{id} not found

--- src/api/router.py
+++ src/api/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 from .models import Routine
 from .shemas import Routine_Schema
@@ -8,18 +8,33 @@ items_router = APIRouter(
     tags=["items"],
 )
 
+
 def get_db():
     db = SessionLocal()
-    try: 
+    try:
         yield db
     finally:
         db.close()
 
 
-@items_router.post('/routine')
+@items_router.post('/routine', status_code=status.HTTP_201_CREATED)
 async def create(request: Routine_Schema, db: Session = Depends(get_db)):
-    new_routine = Routine(title = request.title, category = request.category)
+    new_routine = Routine(title=request.title, category=request.category)
     db.add(new_routine)
     db.commit()
     db.refresh(new_routine)
     return new_routine
+
+
+@items_router.get('/routine', status_code=status.HTTP_200_OK)
+async def get_routines(db: Session = Depends(get_db)):
+    routines = db.query(Routine).all()
+    return routines
+
+
+@items_router.get('/routine/{id}', status_code=status.HTTP_200_OK)
+async def get_routine(id: str, response: Response, db: Session = Depends(get_db)):
+    routine = db.query(Routine).filter(Routine.id == id).first()
+    if not routine:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f'the item with id {id} not found')
+    return routine
